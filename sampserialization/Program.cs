ad798b747ff60d705161121ae41bd944e9d44abc
Sh-uk <121802989+Sh-uk@users.noreply.github.com>
2023-02-21 13:56:13 +0530

    Add files via upload

---
+++ sampserialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+namespace sampserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\SHUKKALI\source\repos\sampserialization\sampserialization\sampserialization.txt";
+            Employee emp = new Employee(1, "shree", 25000);
+            FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine("File created ");
+            Console.ReadKey();
+
+
+
+            //string path = @"C:\Users\SHUKKALI\source\repos\sampserialization\sampserialization\sampserialization.txt";
+            //FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+            //BinaryFormatter bf = new BinaryFormatter();
+            //Employee emp = (Employee)bf.Deserialize(stream);
+            //Console.WriteLine(emp.empSalary);
+            //stream.Close();
+        }
+    }
+    }
+
---
+++ sampserialization/employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+
+
+namespace sampserialization
+{
+    [Serializable]
+    class Employee
+    {
+        public string emName;
+        public int empId;
+        [NonSerialized]
+        public int empSalary;
+
+
+
+
+
+        public Employee(int id, string name, int salary)
+        {
+            this.empId = id;
+            this.emName = name;
+            this.empSalary = salary;
+        }
+
+    }
+}
